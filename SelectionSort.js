ccdbf9f5bb538d39b9c32613a9448b50a2366e27
Robel B. Tamirat <robelbire16@gmail.com>
2023-01-14 13:56:52 +0300

    Create SelectionSort.js

---
+++ SelectionSort.js
@@ -0,0 +1,35 @@
+/**
+ *
+ * select
+ * @param {number[]} arr
+ * @param {number} i
+ * @return {number}
+ *
+ * selectionSort
+ * @param {number[]} arr
+ * @param {number} n
+ */
+class Solution
+{
+  select(arr,i){
+     // code here such that selectionSort() sorts arr[]
+    let min = i
+	  arr.forEach( (item, indx) => indx >= i && arr[indx] < arr[min] && (min = indx))
+	  return min
+  }
+  
+  //Function to sort the array using selection sort algorithm.
+  selectionSort(arr,n){
+   //code here
+   arr.forEach((item, i) => {
+		let minIdx = this.select(arr, i)
+		if( i !== minIdx){
+			arr[i] +=  arr[minIdx];
+			arr[minIdx] = arr[i] - arr[minIdx];
+			arr[i] = arr[i] - arr[minIdx];
+		}
+	})
+	return arr
+	}
+    
+}
