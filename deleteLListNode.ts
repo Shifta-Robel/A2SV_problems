4875d15b7f7e10f07c3a4858057caa5b732ba908
Robel B. Tamirat <robelbire16@gmail.com>
2023-01-29 22:52:26 +0300

    Create deleteLListNode.ts

---
+++ deleteLListNode.ts
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+/**
+ Do not return anything, modify it in-place instead.
+ */
+function deleteNode(node: ListNode | null): void {
+    if (node.next === null) return;
+    node.val = node.next.val;
+    if (node.next.next !== null) {
+        node.next = node.next.next;
+    } else {
+        node.next = null; 
+    }
+};
